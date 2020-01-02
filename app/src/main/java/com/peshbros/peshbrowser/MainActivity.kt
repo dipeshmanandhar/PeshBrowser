42855452246506324acfcde7603f6869828c9afa
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-01-02 17:35:49 -0500

    added fullscreen video support

--- app/src/main/java/com/peshbros/peshbrowser/MainActivity.kt
+++ app/src/main/java/com/peshbros/peshbrowser/MainActivity.kt
@@ -3,16 +3,16 @@ package com.peshbros.peshbrowser
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.KeyEvent
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.view.inputmethod.EditorInfo
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.webkit.WebChromeClient
+import android.widget.*
 
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -21,18 +21,20 @@ fun View.hideKeyboard() {
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fullScreenView: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get reference to editText
+        // get references to Views
         val editText = findViewById<EditText>(R.id.editText)
-        // get reference to buttons
         val goButton = findViewById<Button>(R.id.goButton)
         val backButton = findViewById<Button>(R.id.backButton)
         val forwardButton = findViewById<Button>(R.id.forwardButton)
-        // get reference to webview
         val webView = findViewById<WebView>(R.id.webView)
+        val mainContainer = findViewById<LinearLayout>(R.id.mainContainer)
+        val fullScreenContainer = findViewById<FrameLayout>(R.id.fullScreenContainer)
 
         // set default URL
         webView.loadUrl("https://www.google.com/")
@@ -47,6 +49,25 @@ class MainActivity : AppCompatActivity() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
                 editText.setText(url)
+            }
+        }
+        // set webview fullscreen handler
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                super.onShowCustomView(view, callback)
+                if(view is FrameLayout) {
+                    fullScreenView = view
+                    fullScreenContainer.addView(fullScreenView)
+                    fullScreenContainer.visibility = View.VISIBLE
+                    mainContainer.visibility = View.GONE
+                }
+            }
+
+            override fun onHideCustomView() {
+                super.onHideCustomView()
+                fullScreenContainer.removeView(fullScreenView)
+                fullScreenContainer.visibility = View.GONE
+                mainContainer.visibility = View.VISIBLE
             }
         }
 
